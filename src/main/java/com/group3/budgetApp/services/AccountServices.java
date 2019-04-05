7de1bf46f7a1007294e59c0541eb523dc3a6abad
David Tamulavage <davidtamulavage@zipcodersMBP132.localdomain>
2019-04-05 16:34:35 -0400

    UML started

---
+++ src/main/java/com/group3/budgetApp/services/AccountServices.java
@@ -0,0 +1,8 @@
+package com.group3.budgetApp.services;
+
+public class AccountServices {
+    private javax.persistence.EntityManager em;
+    private javax.persistence.EntityTransaction tx;
+    private javax.persistence.EntityManagerFactory emf;
+    private com.group3.budgetApp.repository.AccountRepository repo;
+}
