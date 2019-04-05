5bf4a1aceea0df10c8bad376ea98e3e2b2f642de
Fenniless <fenniless@gmail.com>
2019-04-05 18:10:24 -0400

    working on account

--- src/main/java/com/group3/budgetApp/repository/AccountRepository.java
+++ src/main/java/com/group3/budgetApp/repository/AccountRepository.java
@@ -4,4 +4,5 @@ import com.group3.budgetApp.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
 }
