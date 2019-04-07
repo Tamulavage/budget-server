32285ab4932997ad0d22990ac01143992bc03f07
Fenniless <fenniless@gmail.com>
2019-04-07 12:10:59 -0400

    account controller

--- src/main/java/com/group3/budgetApp/model/Account.java
+++ src/main/java/com/group3/budgetApp/model/Account.java
@@ -1,12 +1,16 @@
 package com.group3.budgetApp.model;
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Account {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private Double balance;
--- src/main/java/com/group3/budgetApp/services/AccountServices.java
+++ src/main/java/com/group3/budgetApp/services/AccountServices.java
@@ -1,29 +1,29 @@
 package com.group3.budgetApp.services;
-
 import com.group3.budgetApp.model.Account;
+import com.group3.budgetApp.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Persistence;
 @Service
-public class AccountServices {
-    private javax.persistence.EntityManagerFactory emf = Persistence.createEntityManagerFactory("Account");
-    private com.group3.budgetApp.repository.AccountRepository repo;
-    private javax.persistence.EntityManager em = emf.createEntityManager();
-    private javax.persistence.EntityTransaction tx = em.getTransaction();
-
-    public Account createAccount(String name, Double balance, Integer type_id, Integer user_id, String institution_name){
-        Account account = new Account();
-        account.setBalance(balance);
-        account.setName(name);
-        account.setType_id(type_id);
-        account.setUser_id(user_id);
-        account.setInstitution_name(institution_name);
-        tx.begin();
-        em.persist(account);
-        tx.commit();
+public class AccountServices{
+    @Autowired
+    private AccountRepository repo;
+    @Autowired
+    public AccountServices(AccountRepository repo){
+        this.repo = repo;
+    }
+    public Account createAccount(Account account){
+        repo.save(account);
         return account;
     }
-
-
+    public void deleteAccount(Integer id){
+        Account account = repo.getOne(id);
+        if( account != null){
+            repo.delete(account);
+        }
+    }
+    public Account getAccountById(Integer id){
+        return repo.getOne(id);
+    }
 }
 
