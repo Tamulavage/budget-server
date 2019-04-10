39fdeaff4ca26bf8f7fd1664bb591daf666ac083
Fenniless <fenniless@gmail.com>
2019-04-10 14:43:42 -0400

    testing errors

--- src/main/java/com/group3/budgetApp/controller/TransactionController.java
+++ src/main/java/com/group3/budgetApp/controller/TransactionController.java
@@ -17,21 +17,21 @@ public class TransactionController {
     public TransactionController(TransactionServices service) {
         transactionServices = service;
     }
-    
-    @PostMapping("/transaction")
-    public HttpStatus depositTransaction(@RequestBody Transaction d) {
-        
-        try {
-            transactionServices.createDeposit(d);
-            return HttpStatus.CREATED;
-        } catch (IllegalArgumentException iae) {
-            System.err.println(iae.getMessage());
-            return HttpStatus.NOT_ACCEPTABLE;
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            return HttpStatus.SERVICE_UNAVAILABLE;
-        }
-    }
+//
+//    @PostMapping("/transaction")
+//    public HttpStatus depositTransaction(@RequestBody Transaction d) {
+//
+//        try {
+//            transactionServices.createDeposit(d);
+//            return HttpStatus.CREATED;
+//        } catch (IllegalArgumentException iae) {
+//            System.err.println(iae.getMessage());
+//            return HttpStatus.NOT_ACCEPTABLE;
+//        } catch (Exception e) {
+//            System.err.println(e.getMessage());
+//            return HttpStatus.SERVICE_UNAVAILABLE;
+//        }
+//    }
     
     @PostMapping("/transaction")
     public HttpStatus withdrawalTransaction(@RequestBody Transaction w) {
