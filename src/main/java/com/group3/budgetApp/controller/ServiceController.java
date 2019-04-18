5b8b5ea4e5c6332a2f6db5c404e70bf00de068d3
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-04-18 15:00:45 -0400

    Changed cross origin to heroku app

--- src/main/java/com/group3/budgetApp/controller/ServiceController.java
+++ src/main/java/com/group3/budgetApp/controller/ServiceController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://zipbudget.herokuapp.com")
 @RequestMapping("/budget")
 public class ServiceController {
     private AccountServices accountService;
--- src/main/java/com/group3/budgetApp/controller/TransactionController.java
+++ src/main/java/com/group3/budgetApp/controller/TransactionController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://zipbudget.herokuapp.com")
 @RequestMapping("/budget")
 public class TransactionController {
     
