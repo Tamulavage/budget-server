67a7a4920c1b0131c74ce76b2577ed3fa7dcc1f5
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-04-18 17:24:27 -0400

    Corrected transaction type CO

--- src/main/java/com/group3/budgetApp/controller/TransactionTypeController.java
+++ src/main/java/com/group3/budgetApp/controller/TransactionTypeController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://budgetapp-client.herokuapp.com")
 @RequestMapping("/budget")
 public class TransactionTypeController {
 
