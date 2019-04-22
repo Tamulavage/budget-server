4053f54c9440a584452a618a0166022039e02f45
Mark Moll <mdmoll@gmail.com>
2019-04-22 09:25:03 -0400

    Added distinct logic to prevent duplicate transactions appearing in list

--- src/main/java/com/group3/budgetApp/repository/TransactionRepository.java
+++ src/main/java/com/group3/budgetApp/repository/TransactionRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-    @Query(value = "SELECT * FROM Transaction t INNER JOIN Account a ON t.from_account_id = a.id OR t.to_account_id = a.id INNER JOIN PROFILE p ON p.user_id = a.user_id WHERE p.user_id = ?1 ORDER BY transaction_dt DESC", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT * FROM Transaction t INNER JOIN Account a ON t.from_account_id = a.id OR t.to_account_id = a.id INNER JOIN PROFILE p ON p.user_id = a.user_id WHERE p.user_id = ?1 ORDER BY transaction_dt DESC", nativeQuery = true)
     List<Transaction> findAllByUserId(Integer userId);
     List<Transaction> findAllByFromAccountIdOrToAccountId(Integer acctId, Integer acctId2);
 //    List<Transaction> findAllByToAccountIdOrderByTransactionDtDesc(Integer toId);
